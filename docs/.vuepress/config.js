1e80c74d3fe34c128731bf0f8a149abe8be371ed
unknown <audgkwns44@gmail.com>
2020-06-28 19:23:46 +0900

    follow the guide: ready for multi-lang page / navi

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,26 @@ module.exports={
     locales : {
         '/':{
             lang: 'en-US',
-            title : "Hajun's summary of learnt",
+            title : "Hajun's study-blog",
             description : 'Summary / Learning Processes of my studying'
         },
         '/kr/':{
             lang: 'ko-KR',
             title : "하준's 학습블로그",
             description : '공부한 내용들과 학습 과정을 정리해놓기 위한 블로그입니다.'
+        }
+    },
+
+    themeConfig:{
+        locales:{
+            '/':{
+                selectText: 'Languages',
+                label: 'English'
+            },
+            '/kr/':{
+                selectText: '언어',
+                label: '한국어'
+            }
         }
     }
 }
