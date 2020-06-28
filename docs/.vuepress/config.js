deff2e8d82ab95a806c56a4cbbb07983514cf462
unknown <audgkwns44@gmail.com>
2020-06-28 19:58:21 +0900

    languages order change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,26 @@
 module.exports={
     locales : {
         '/':{
-            lang: 'en-US',
-            title : "Hajun's study-blog",
-            description : 'Summary / Learning Processes of my studying'
-        },
-        '/kr/':{
             lang: 'ko-KR',
             title : "하준's 학습블로그",
             description : '공부한 내용들과 학습 과정을 정리해놓기 위한 블로그입니다.'
+        },
+        '/eng/':{
+            lang: 'en-US',
+            title : "Hajun's study-blog",
+            description : 'Summary / Learning Processes of my studying'
         }
     },
 
     themeConfig:{
         locales:{
             '/':{
-                selectText: 'Languages',
-                label: 'English'
-            },
-            '/kr/':{
                 selectText: '언어',
                 label: '한국어'
+            },
+            '/kr/':{
+                selectText: 'languages',
+                label: 'English'
             }
         }
     }
