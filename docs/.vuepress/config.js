9b98c46489fb94f57db889f48a4c69970f380c03
unknown <audgkwns44@gmail.com>
2020-06-28 19:12:31 +0900

    follow the guide : KR ver making

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,14 @@
 module.exports={
-    title : "Hajun's summary of learnt",
-    description : 'Summary / Learning Processes of my studying'
+    locales : {
+        '/':{
+            lang: 'en-US',
+            title : "Hajun's summary of learnt",
+            description : 'Summary / Learning Processes of my studying'
+        },
+        '/kr/':{
+            lang: 'ko-KR',
+            title : "하준's 학습블로그",
+            description : '공부한 내용들과 학습 과정을 정리해놓기 위한 블로그입니다.'
+        }
+    }
 }
