9628b7c552e2e8f1e2241c5f3f0e5aee535e0a05
unknown <audgkwns44@gmail.com>
2020-06-30 07:48:02 +0900

    List(link) update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,26 +17,86 @@ module.exports={
         locales:{
             '/':{
                 selectText: '언어(Languages)',
-                label: '한국어'
+                label: '한국어',
+                
+                nav :[
+                    { text: '홈', link: '/'},
+                    {
+                        text: '공부한 것들',
+                        items: [
+                            { text: '뷰프레스(VuePress)', items:[
+                                { text: '뷰프레스(VuePress)', link: '/vuepress/'}
+                            ]},
+                            { text: '프로그래밍 언어', items :
+                                [
+                                    { text: 'HTML', link: '/languages/html/'},
+                                    { text: 'Python', link: '/languages/python/'},
+                                    { text: 'JS', link: '/languages/javaScript/'}
+                                ]
+                            },
+                            { text: '메이킹', items:[
+                                { text: '아두이노', link: '/making/arduino/'},
+                                { text: '프로세싱', link: '/making/processing/'}
+                            ]}
+                        ]
+                    },
+                    {
+                        text: '프로젝트',
+                        items: [
+                            {text: '짐벌만들기', link: '/projects/gimabl/'},
+                            {text: '체크카드 만들기', link: '/projects/debit-card/'}
+                        ]
+                    },
+                    {
+                        text: '엔지니어링',
+                        items: [
+                            {text: '기본', link: '/engineering/basic/'}
+                        ]
+                    }
+                ],
             },
+
             '/eng/':{
                 selectText: 'Languages(언어)',
-                label: 'English'
+                label: 'English',
+                
+                nav :[
+                    { text: 'Home', link: '/'},
+                    {
+                        text: 'Learnt',
+                        items: [
+                            { text: 'VuePress', items:[
+                                { text: 'VuePress', link: '/vuepress/'}
+                            ]},
+                            { text: 'Programing Languages', items :
+                                [
+                                    { text: 'HTML', link: '/languages/html/'},
+                                    { text: 'Python', link: '/languages/python/'},
+                                    { text: 'JS', link: '/languages/javaScript/'}
+                                ]
+                            },
+                            { text: 'making', items:[
+                                { text: 'arduino', link: '/making/arduino/'},
+                                { text: 'processing', link: '/making/processing/'}
+                            ]}
+                        ]
+                    },
+                    {
+                        text: 'Projects',
+                        items: [
+                            {text: 'making Gimbal', link: '/projects/gimabl/'},
+                            {text: 'making DebitCard', link: '/projects/debit-card/'}
+                        ]
+                    },
+                    {
+                        text: 'Engineering',
+                        items: [
+                            {text: 'basic', link: '/engineering/basic/'}
+                        ]
+                    }
+                ],
             }
         },
-
-        nav :[
-            { text: 'Home', link: '/'},
-            {
-                text: 'Learnt',
-                items: [
-                    { text: 'HTML', link: '/html/'},
-                    { text: 'Python', link: '/python/'},
-                    { text: 'JS', link: 'javaScript'}
-                ]
-            }
-        ],
-
         plugins: ['@vuepress/back-to-top'],
     }
 }
